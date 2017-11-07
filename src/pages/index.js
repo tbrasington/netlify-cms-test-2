6a0de89e1ba53846ba339a6b58f879d9bbc47091
Thomas Brasington <tom@2c2d.co.uk>
2017-11-07 15:20:28 +0000

    more login flow attempts

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,16 @@ import Helmet from 'react-helmet';
 export default class IndexPage extends React.Component {
   componentDidMount() {
     if (window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
+      window.netlifyIdentity.on('init', user => {
         if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
           });
         }
       });
     }
   }
+
 
   render() { 
     return (<div>
