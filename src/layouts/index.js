6cd385ce647ebb07a61b58cdf99c945a8bd0e654
Thomas Brasington <tom@2c2d.co.uk>
2017-11-07 11:04:02 +0000

    another way of doing this

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -43,19 +43,6 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <script type="application/script">{`
-    {
-      if (window.netlifyIdentity) {
-        window.netlifyIdentity.on("init", user => {
-          if (!user) {
-            window.netlifyIdentity.on("login", () => {
-              document.location.href = "/admin/";
-            });
-          }
-        });
-      }
-    }
-`}</script>
     <Header />
     <div
       style={{
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+export default class IndexPage extends React.Component {
+  componentDidMount() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+  }
 
-export default IndexPage
+  render() { 
+    return (<div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>)
+   } 
+};
