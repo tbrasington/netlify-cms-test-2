7d32e9c439184b0191d3f8c83ae19d0a02a7b6da
Thomas Brasington <tom@2c2d.co.uk>
2017-11-07 14:31:33 +0000

    setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Helmet from 'react-helmet';
 
 export default class IndexPage extends React.Component {
   componentDidMount() {
     if (window.netlifyIdentity) {
-      window.netlifyIdentity.on('init', user => {
+      window.netlifyIdentity.on("init", user => {
         if (!user) {
-          window.netlifyIdentity.on('login', () => {
-            document.location.href = '/admin/';
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
           });
         }
       });
@@ -16,6 +17,9 @@ export default class IndexPage extends React.Component {
 
   render() { 
     return (<div>
+      <Helmet>
+      <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+    </Helmet>
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
